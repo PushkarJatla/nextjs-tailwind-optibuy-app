f279a7e216046df0ee3a68f2c04486239bb144e4
PushkarJatla <pushkarjatla02@gmail.com>
2025-07-09 11:54:44 -0700

    added responsiveness to navbar

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -165,7 +165,7 @@ export default function HomePage() {
 
     return (
         <div className="min-h-screen flex flex-col bg-gradient-to-br from-emerald-50 via-green-50 to-white">
-            <header className="bg-emerald-200 shadow-md px-6 py-2 flex justify-between items-center text-green-900 font-semibold">
+            <header className="bg-emerald-200 shadow-md px-6 py-2 flex flex-col md:flex-row  justify-between items-center text-green-900 font-semibold">
                 <div className="flex items-center gap-3">
                     <img src="/shopping-bag.png" alt="CompareWise Logo" className="w-10 h-10 object-contain" />
 
